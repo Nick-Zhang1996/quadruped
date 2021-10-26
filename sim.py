5da440ef164a92e4d7de4fbf97f6b27c3cfb269f
Nick Zhang <zzhang615@ae.gatech.edu>
2021-10-25 23:20:26 -0400

    minor update on display and unit

--- sim.py
+++ sim.py
@@ -4,6 +4,12 @@ import pygame
 import pymunk
 import pymunk.pygame_util
 
+import random
+random.seed(1)
+
+# NOTE: to make display with pygame simpler unit here used is scaled
+# distance: cm, g: cm/s^2, force: N/100
+
 class Physics:
     def __init__(self):
         self.width = 800
@@ -18,14 +24,29 @@ class Physics:
 
 
         self.space = pymunk.Space()
-        self.space.gravity = (0.0, 9.8)
+        self.space.gravity = (0.0, -9.8*100)
         self.addGround()
+
+        # DEBUG
+        self.addBall()
+    def addBall(self):
+        """Add a ball to the given space at a random position"""
+        mass = 3
+        radius = 25
+        inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
+        body = pymunk.Body(mass, inertia)
+        x = random.randint(120,300)
+        body.position = x, self.ground_height + 100
+        shape = pymunk.Circle(body, radius, (0,0))
+        shape.friction = 1
+        self.space.add(body, shape)
+        return shape
 
     def addGround(self):
         radius = 5
         # (x,y) origin at upper left, x+ right y+down
-        ground_height = 100
-        segment = pymunk.Segment(self.space.static_body, (100, ground_height), (self.width/2,ground_height), radius)
+        self.ground_height = ground_height = 100
+        segment = pymunk.Segment(self.space.static_body, (0, ground_height), (self.width,ground_height), radius)
         segment.friction = 0.9
         #segment.collision_type = None
         self.space.add(segment)
